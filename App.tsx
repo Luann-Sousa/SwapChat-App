bc8dea58489525163b5253c4fe682c31043f3d17
Luann244 <weversonluan17@gmail.com>
2021-11-23 22:39:55 -0300

    setup da aplicação.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { HomeScreen } from './src/screens/Home';
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +12,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <>
+      <View style={styles.container}>
+        <StatusBar translucent />
+        <HomeScreen />
+      </View>
+    </>
+  );
+}
