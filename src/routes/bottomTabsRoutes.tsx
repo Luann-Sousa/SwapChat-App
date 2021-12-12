7b0ba5e8ab093fdc6fdfba04a4b4b8c942769340
weversonluan17@gmail.com <weversonluan17@gmail.com>
2021-12-12 00:16:07 -0300

    refact: component container

--- src/routes/bottomTabsRoutes.tsx
+++ src/routes/bottomTabsRoutes.tsx
@@ -1,26 +1,10 @@
 /* eslint-disable no-nested-ternary */
 import * as React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
 import FriendsSVG from '../assets/icon/friends.svg';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import { MatchScreen } from '../screens/Match';
+import { FriendsScreen } from '../screens/Friends';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -33,14 +17,14 @@ export function BottomTabsRoutes() {
     >
       <BottomTabs.Screen
         name="Home"
-        component={HomeScreen}
+        component={MatchScreen}
         options={{
           tabBarIcon: ({ size, color }) => <MaterialIcons name="home" size={size} color={color} />,
         }}
       />
       <BottomTabs.Screen
         name="Friends"
-        component={SettingsScreen}
+        component={FriendsScreen}
         options={{
           tabBarIcon: () => <FriendsSVG width={24} height={24} />,
         }}
--- src/screens/Friends/styles.ts
+++ src/screens/Friends/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  width: 100%;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export const Title = styled.Text``;
