e83bf331a9d3940b53fbcb34156b1ce2f9bb2152
weversonluan17@gmail.com <weversonluan17@gmail.com>
2021-12-11 21:45:43 -0300

    create: screen signinUp  criada tela de cadastro

---
+++ src/screens/SignInUp/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export function SignInUp() {
+  return (
+    <Container>
+      <Title> ProgramadorWL</Title>
+    </Container>
+  );
+}
