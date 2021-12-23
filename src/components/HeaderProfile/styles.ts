fdf1e59a540fc8d0c7a69fe23d41cdfc9aa41793
weversonluan17@gmail.com <weversonluan17@gmail.com>
2021-12-23 18:01:27 -0300

    create: component headerProfile foi finalizado

--- src/components/HeaderProfile/styles.ts
+++ src/components/HeaderProfile/styles.ts
@@ -5,16 +5,38 @@ export const Container = styled.View`
   width: 100%;
   height: 80px;
 
-  border: 1px solid red;
+  /* border: 1px solid red; */
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 export const WrapperHeader = styled.View`
   margin-top: 10px;
   width: 250px;
   height: 60px;
-  border: 1px solid blue;
+  /* border: 1px solid blue; */
+  flex-direction: row;
+  align-items: center;
+  /* justify-content: space-between; */
+  margin-right: 10px;
+`;
+
+export const WrapperHeaderIcon = styled.View`
+  margin-top: 10px;
+  width: 100px;
+  height: 60px;
+  /* border: 1px solid blue; */
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
 `;
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-family: 'Roboto';
+
+  font-weight: 900;
+  font-size: 24px;
+  color: #403d3d;
+  margin-left: 20px;
+`;
--- src/screens/ChatAll/styles.ts
+++ src/screens/ChatAll/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.ScrollView`
   width: 100%;
   flex: 1;
-  padding: 24px;
+  padding: 14px;
 `;
 
 export const Title = styled.Text``;
