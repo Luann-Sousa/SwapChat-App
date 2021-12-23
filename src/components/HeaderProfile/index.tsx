6b60d1c3fa903da32e9b9763203909492e70de11
weversonluan17@gmail.com <weversonluan17@gmail.com>
2021-12-23 17:23:23 -0300

    create: component headerProfile

---
+++ src/components/HeaderProfile/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export function HeaderProfile() {
+  return (
+    <Container>
+      <Title>Olá, Programador !</Title>
+    </Container>
+  );
+}
