27eca5c3ea036f0584e991c6a36b315478a9a73c
weversonluan17@gmail.com <weversonluan17@gmail.com>
2021-12-28 18:16:10 -0300

    create: o componente SearchInput esta sendo criado

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export function SearchInput() {
+  return (
+    <Container>
+      <Title>Olá, programador Luann</Title>
+    </Container>
+  );
+}
