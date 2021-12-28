3d6d60fdb0b2f6fc8a16f160de266eee2fc4d0b7
weversonluan17@gmail.com <weversonluan17@gmail.com>
2021-12-28 18:30:03 -0300

    create: o componente SearchInput foi finalizado

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Container, Title } from './styles';
+import { FontAwesome } from '@expo/vector-icons';
+import { Container, Search } from './styles';
 
 export function SearchInput() {
   return (
     <Container>
-      <Title>Olá, programador Luann</Title>
+      {/* <Title>Olá, programador Luann</Title> */}
+      <Search placeholder="Pesqueisar" />
+      <FontAwesome
+        name="search"
+        size={18}
+        color="#a2a2a2"
+        style={{ position: 'absolute', marginTop: 10, marginLeft: 16 }}
+      />
     </Container>
   );
 }
--- src/screens/ChatAll/index.tsx
+++ src/screens/ChatAll/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Container, Title } from './styles';
 
 import { HeaderProfile } from '../../components/HeaderProfile';
+import { SearchInput } from '../../components/SearchInput';
 
 export function ChatAll() {
   return (
     <Container>
       <HeaderProfile />
-      <Title>Welcome is Screen Chat !</Title>
+      <SearchInput />
     </Container>
   );
 }
